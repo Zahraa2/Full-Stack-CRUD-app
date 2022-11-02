e2bdac0f228e49d4de2efe2f0e9b5dd06edf9e45
Zahraa2 <zahraavelo@gmail.com>
2022-11-02 02:11:13 +0200

    handled showing inspection data

--- Angular/src/app/inspection/show-inspection/show-inspection.component.ts
+++ Angular/src/app/inspection/show-inspection/show-inspection.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { InspectionApiService } from 'src/app/services/inspection-api.service';
 
 @Component({
   selector: 'app-show-inspection',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowInspectionComponent implements OnInit {
 
-  constructor() { }
+
+  
+  
+  inspectionList$!:Observable<any[]>;
+  inspectionTypesList$!:Observable<any[]>;
+  inspectionTypesList:any=[];
+
+  // Map to display data associate with foreign keys
+  inspectionTypesMap:Map<number, string> = new Map()
+
+ 
+  constructor(private service: InspectionApiService) { }
 
   ngOnInit(): void {
+    this.inspectionList$ = this.service.getInspectionList();
+    this.inspectionTypesList$ = this.service.getInspectionTypesList();
+    this.refreshInspectionTypesMap();
+
   }
 
+
+  refreshInspectionTypesMap() {
+    this.service.getInspectionTypesList().subscribe(data => {
+      this.inspectionTypesList = data;
+
+      for(let i = 0; i < data.length; i++)
+      {
+        this.inspectionTypesMap.set(this.inspectionTypesList[i].id, this.inspectionTypesList[i].inspectionName);
+      }
+    })
+  }
 }
